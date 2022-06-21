74a141314f7060dfec85274a4f388efc8037c9b2
sparsh kumar <sparshkumar14998@gmail.com>
2022-06-22 00:52:55 +0530

    Using Bugsnag to Log errors in PHP codebase.

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use MyApp\Game\Player\Player;
 use MyApp\Game\Game;
 use MyApp\Game\GameController;
 
+require_once realpath("bugsnag.phar");
+require_once realpath("guzzle.phar");
 require_once realpath("vendor/autoload.php");
 
 $db = new Database();
@@ -12,4 +14,11 @@ $player = new Player();
 $game = new Game();
 $gameController = new GameController();
 
+$bugsnag = Bugsnag\Client::make('e67c70d5585993301f4ac92650bb4fd7');
+Bugsnag\Handler::register($bugsnag);
 
+try {
+    throw new Exception('Another Exception by Sparsh');
+} catch (Exception $exception) {
+    $bugsnag->notifyException($exception);
+}
